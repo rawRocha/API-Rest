9b52ea0b53c42f9ab76ee1f3e5164e811547f0b7
Rawlinson Rocha <rawlinson.95@gmail.com>
2024-11-04 19:11:00 -0300

    adicionando arquivos

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ class App {
   middlewares() {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
-    this.app.use(express.static(resolve(__dirname, '..', 'uploads', 'imgs')));
+    this.app.use('/imgs/', express.static(resolve(__dirname, '..', 'uploads', 'imgs')));
   }
 
   routes() {
