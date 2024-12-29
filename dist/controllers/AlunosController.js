af0b669f4efefdbb408c76812eecabb90b9aa7d9
Rawlinson Rocha <rawlinson.95@gmail.com>
2024-12-29 17:18:41 -0300

    Alterado appConfig

---
+++ dist/controllers/AlunosController.js
@@ -0,0 +1,119 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _Aluno = require('../models/Aluno'); var _Aluno2 = _interopRequireDefault(_Aluno);
+var _Foto = require('../models/Foto'); var _Foto2 = _interopRequireDefault(_Foto);
+
+class AlunoController {
+  async index(req, res) {
+    const alunos = await _Aluno2.default.findAll({
+      attributes: ['id', 'nome', 'sobrenome', 'email', 'idade', 'peso', 'altura'],
+      order: [['id', 'DESC'], [_Foto2.default, 'id', 'DESC']],
+      include: {
+        model: _Foto2.default,
+        attributes: ['url', 'originalname', 'filename'],
+      },
+    });
+    res.json(alunos);
+  }
+
+  async store(req, res) {
+    try {
+      const newAluno = await _Aluno2.default.create(req.body);
+
+      return res.json(newAluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Missing id'],
+        });
+      }
+
+      const aluno = await _Aluno2.default.findByPk(id, {
+        attributes: ['id', 'nome', 'sobrenome', 'email', 'idade', 'peso', 'altura'],
+        order: [['id', 'DESC'], [_Foto2.default, 'id', 'DESC']],
+        include: {
+          model: _Foto2.default,
+          attributes: ['url', 'originalname', 'filename'],
+        },
+      });
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Missing student'],
+        });
+      }
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Missing id'],
+        });
+      }
+
+      const aluno = await _Aluno2.default.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Missing student'],
+        });
+      }
+
+      const alunoEdit = await aluno.update(req.body);
+
+      return res.json(alunoEdit);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Missing id'],
+        });
+      }
+
+      const aluno = await _Aluno2.default.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Missing student'],
+        });
+      }
+
+      await aluno.destroy();
+
+      return res.json({ delete: true });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+}
+
+exports. default = new AlunoController();
